6a206cdd3bc570fc43ffd07050f4f72a4d1b7cfa
moridi <m.moridi.2009@gmail.com>
2019-06-05 20:01:07 +0430

    Make the HttpHandler singleton

--- HttpHandler.py
+++ HttpHandler.py
@@ -1,9 +1,22 @@
 import requests 
 
 def HttpHandler():
+    __instance = None
+    @staticmethod
+    def getInstance():
+        """ Static access method. """
+        if HttpHandler.__instance == None:
+            HttpHandler()
+        return HttpHandler.__instance
+
     def __init__(self):
-        self.url = "http://142.93.134.194:8088/api/attendance"
-    
+        """ Virtually private constructor. """
+        if HttpHandler.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            HttpHandler.__instance = self
+            self.url = "http://142.93.134.194:8088/api/attendance"
+        
     def getExams(self):
         exams = requests.get(url = self.url) 
         return exams.json()
